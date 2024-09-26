76fba39618f97c86505760495e7a9556066e2fa6
Jheovanny <jheovanny@hyerdev.com>
2024-09-25 22:56:51 -0300

    correção link

--- web/src/components/Landing/Sections/Introduction/index.tsx
+++ web/src/components/Landing/Sections/Introduction/index.tsx
@@ -3,14 +3,16 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import useContentStore from "@/store/contentStore";
 import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const IntroductionSection = () => {
   const { siteData, loading } = useContentStore();
+  const navigate = useNavigate();
   const contactRef = useRef<HTMLDivElement | null>(null);
 
   return (
     <div className="w-full flex flex-col gap-2 lg:flex-row lg:h-[620px]">
-      <div className="w-full h-full pl-[60px] flex flex-col gap-14 justify-start lg:w-[50%]">
+      <div className="w-full h-full lg:pl-[60px] p-5 flex flex-col gap-14 justify-start lg:w-[50%]">
         <h1 className="font-bold text-5xl">
           {!loading ? siteData.principal?.title : <Skeleton />}
         </h1>
@@ -18,16 +20,16 @@ const IntroductionSection = () => {
           <span className="max-w-[600px] text-muted-foreground md:text-xl">
             {!loading ? siteData.principal?.subTitle : <Skeleton />}
           </span>
-          <div className="flex gap-4">
+          <div className="flex gap-4 flex-col md:flex-row lg:flex-row">
             <Button
-              className="bg-blue-500 w-[120px] hover:bg-blue-600"
-              onClick={() => (window.location.href = "/about")}
+              className="bg-blue-500 lg:w-[120px] w-full hover:bg-blue-600"
+              onClick={() => navigate("/about")}
             >
               Saiba mais
             </Button>
             <Button
               variant={"outline"}
-              className="w-[120px]"
+              className="lg:w-[120px] w-full"
               onClick={() => {
                 contactRef.current?.scrollIntoView({
                   behavior: "smooth",
@@ -40,11 +42,11 @@ const IntroductionSection = () => {
         </div>
       </div>
       <div className="w-full h-full flex justify-center items-center lg:w-[50%]">
-        <div className="relative w-full h-full rounded-lg">
+        <div className="relative w-full h-full p-5 lg:p-0">
           {!loading && siteData.principal.image ? (
             <img
               src={siteData.principal.image}
-              className="object-contain w-full h-full rounded-lg"
+              className="object-contain size-full rounded-lg"
             />
           ) : (
             <Skeleton className="h-[600px] w-[900px] rounded-lg" />
