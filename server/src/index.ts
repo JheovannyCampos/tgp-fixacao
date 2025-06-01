5d25445d42c006fc351f14b186eff8c1ca4eaac8
Jheovanny <jheovanny06@hotmail.com>
2025-06-01 15:22:37 -0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -81,9 +81,7 @@ app.get("/data", async (req, res) => {
     ]);
 
     const principalResultsWithUrls = {
-      logo: principalData[0]?.logo
-        ? getHighQualityUrl(principalData[0].logo)
-        : null,
+      logo: principalData[0]?.logo ? principalData[0].logo : null,
       title: principalData[0]?.title || "",
       subTitle: principalData[0]?.subTitle || "",
       image: principalData[0]?.image
